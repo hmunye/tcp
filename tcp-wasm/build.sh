e2a1b7bc13e421b0da46442d66dfdbadb3f8cfce
Huseen <136820327+hmunye@users.noreply.github.com>
2025-08-25 14:42:04 -0400

    refactor: created release-wasm profile so  can be used

--- tcp-wasm/build.sh
+++ tcp-wasm/build.sh
@@ -3,12 +3,11 @@
 set -euo pipefail
 
 # Compile library into `.wasm` binary
-cargo b --target wasm32-unknown-unknown --release
+cargo b --target wasm32-unknown-unknown --profile release-wasm
 
 # Runs `wasm-bindgen` to generate JavaScript bindings
-wasm-bindgen ../target/wasm32-unknown-unknown/release/tcp_wasm.wasm \
+wasm-bindgen ../target/wasm32-unknown-unknown/release-wasm/tcp_wasm.wasm \
   --out-dir ./frontend/pkg \
-  --out-name tcp_wasm \
   --target web \
   --remove-name-section \
   --remove-producers-section
